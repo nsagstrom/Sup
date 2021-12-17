2a85bf8f6c26e7a6c0f1f4c2e379245e59ca29e8
nsagstrom <n.sagstrom@gmail.com>
2021-12-17 13:46:04 +0100

    Initial commit

---
+++ src/test1/Start.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test1;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+public class Start {
+    
+    private static InfDB idb;
+    
+    public static void main(String[] args) throws InfException {
+        try {
+            idb = new InfDB("mibdb", "3306", "mibdba", "mibkey");
+        } catch (InfException ex) {
+            Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+}
